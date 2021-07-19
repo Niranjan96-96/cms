df11dbe95a7ca5f77c3a804798fd19e8b5f1bc2d
Niranjan <niranjanreddy9a1@gmail.com>
2021-07-19 13:20:29 +0530

    up

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const toggleButton=document.getElementsByClassName('toggle-button')[0]
 const content=document.getElementsByClassName('content')[0]
 toggleButton.addEventListener('click',()=>{
-    contentLinks.classList.toggle('active')
+    content.classList.toggle('active')
 })
